4677f2e72f585907c96f7249d598305e510813b9
Samuel Fox <samuel.fox@mymail.champlain.edu>
2022-09-26 14:16:14 -0400

    priority_queue

--- examples/catchthecat/Cat.h
+++ examples/catchthecat/Cat.h
@@ -2,9 +2,14 @@
 #define CAT_H
 
 #include "Agent.h"
+#include <queue>
 
-class Cat: public Agent {
-public:
+class Cat: public Agent
+{
+ private:
+  std::priority_queue queue;
+
+ public:
     explicit Cat():Agent(){};
     Point2D Move(World *) override;
 };
